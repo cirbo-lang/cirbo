73493289a121b504e532d2d64158af33d686b28d
Martin Atkins <mart@degeneration.co.uk>
2017-09-10 14:54:07 -0700

    ast: distinguish between BidiLeader and BidiFollower
    Although bidirectional pins operate as both inputs and outputs
    electrically, there is typically a higher-level relationship between
    devices that use bidirectional signaling where some devices drive the
    conversation while others just participate.
    
    Electrically this makes no difference, and thus these "directions" are
    weaker than Input and Output, but this distinction serves as a useful
    hint about the relationships between different devices in a system that
    can be useful in generating diagrams and other materials that are at
    the "schematic" level of abstraction.

--- ast/terminal.go
+++ ast/terminal.go
@@ -19,9 +19,29 @@ const (
 type TerminalDir rune
 
 const (
-	Bidirectional TerminalDir = 0
-	Input         TerminalDir = 'I'
-	Output        TerminalDir = 'O'
+	Undirected TerminalDir = 0
+	Input      TerminalDir = 'I'
+	Output     TerminalDir = 'O'
+
+	// BidiLeader is a terminal direction that indicates that it operates
+	// both as an input and an output but that the associated device is
+	// an "bus leader", meaning that it initiates transactions.
+	//
+	// For example, in a typical application with a microcontroller requesting
+	// data from a peripheral using I2C, the microcontroller would be a
+	// BidiLeader and the peripheral a BidiFollower, since the peripheral
+	// remains inactive until the microcontroller requests data.
+	BidiLeader TerminalDir = 'L'
+
+	// BidiFollower is a terminal direction that indicates that it operates
+	// both as an input and an output but that the associated device is
+	// a "bus follower", meaning that it participates in transactions
+	// initiated by other devices on the bus but does not itself begin
+	// transactions.
+	//
+	// If a device participates both as a leader and a follower, BidiLeader
+	// should be considered to take precedence.
+	BidiFollower TerminalDir = 'F'
 )
 
 type TerminalOutputType rune
