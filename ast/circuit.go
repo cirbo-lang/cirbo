47e5882fda0cfdecb7f679e68b29a5a17fc6988f
Martin Atkins <mart@degeneration.co.uk>
2017-09-10 17:49:15 -0700

    ast: New NodeDecl interface
    This is an extension of Node implemented by nodes that have a declaration
    portion that is a subset of their whole range.
    
    This extension is for now just implemented by Circuit, but will be
    implemented by all of the declaration blocks eventually.

--- ast/circuit.go
+++ ast/circuit.go
@@ -1,11 +1,22 @@
 package ast
 
+import (
+	"github.com/cirbo-lang/cirbo/source"
+)
+
 type Circuit struct {
 	WithRange
 	Name string
+	Body []Node
+
+	HeaderRange source.Range
 }
 
 func (n *Circuit) walkChildNodes(cb internalWalkFunc) {
 	// TODO: Implement child nodes
 	panic("walkChildNodes not implemented for Circuit")
+}
+
+func (n *Circuit) DeclRange() source.Range {
+	return n.HeaderRange
 }
--- ast/node.go
+++ ast/node.go
@@ -21,3 +21,18 @@ type WithRange struct {
 func (n *WithRange) SourceRange() source.Range {
 	return n.Range
 }
+
+// NodeDecl is an extension of Node that adds a DeclRange method.
+type NodeDecl interface {
+	Node
+
+	// DeclRange returns the range that covers the part of a definition
+	// node that serves as a declaration.
+	//
+	// For complex constructs that include nested statement blocks, this
+	// returns just the range around the tokens that introduce the block,
+	// and not the block itself. This result is, therefore, a better
+	// range to use when presenting diagnostics relating to declarations
+	// so as to focus the user's attention on the declaration portion.
+	DeclRange() source.Range
+}
