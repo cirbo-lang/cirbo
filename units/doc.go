cee11351a29f6fe6378be90ab01b6209d93a42fb
Martin Atkins <mart@degeneration.co.uk>
2017-09-16 08:45:12 -0700

    units: Clarify some key limitations in the docs

--- units/doc.go
+++ units/doc.go
@@ -1,5 +1,9 @@
 // Package units deals with the system of units used by Cirbo.
 //
 // It is not intended as a general-purpose units library; it's tailored for
-// the needs of the Cirbo language in particular.
+// the needs of the Cirbo language. In particular, it considers angle to be
+// a base quantity (contrary to SI standards) and treats decimal degrees as
+// its primary unit (consistent with engineering practice). It also supports
+// only integer powers, and thus cannot represent units such as
+// "square root of seconds".
 package units
