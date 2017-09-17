1d3d1cf66bb03499321af6a12756406010611dfd
Martin Atkins <mart@degeneration.co.uk>
2017-09-17 16:09:23 -0700

    debug-tools/cirbo-ast: little tool for printing out ASTs
    This tool is to help with debugging the parser and AST by showing a
    compact representation of the AST for any given source file.

---
+++ debug-tools/cirbo-ast/main.go
@@ -0,0 +1,103 @@
+package main
+
+import (
+	"bytes"
+	"errors"
+	"flag"
+	"fmt"
+	"os"
+	"path/filepath"
+	"reflect"
+	"strings"
+
+	"github.com/cirbo-lang/cirbo/source"
+
+	"github.com/cirbo-lang/cirbo/ast"
+	"github.com/cirbo-lang/cirbo/parser"
+)
+
+func main() {
+	err := realMain(os.Args[1:])
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "%s\n\n", err)
+		os.Exit(1)
+	}
+}
+
+func realMain(args []string) error {
+	fl := flag.NewFlagSet("cirbo-ast", flag.ExitOnError)
+	err := fl.Parse(args)
+	if err != nil {
+		return err
+	}
+	args = fl.Args()
+
+	if len(args) != 1 {
+		fl.Usage()
+		os.Exit(1)
+	}
+
+	fullPath := args[0]
+	rootPath := filepath.Dir(fullPath)
+	fn := filepath.Base(fullPath)
+
+	p := parser.NewParser(rootPath)
+	f, diags := p.ParseFile(fn)
+
+	ast.Walk(f, &walker{})
+
+	if len(diags) > 0 {
+		os.Stderr.WriteString("\n")
+		for _, diag := range diags {
+			fmt.Fprintf(os.Stderr, "- %s\n", diag.String())
+		}
+		os.Stderr.WriteString("\n")
+		return errors.New("There were some errors during parsing, as shown above.")
+	}
+	return nil
+}
+
+type walker struct {
+	Depth int
+}
+
+func (w *walker) EnterNode(node ast.Node) bool {
+	buf := &bytes.Buffer{}
+	buf.WriteString(strings.Repeat("  ", w.Depth))
+	buf.WriteString(fmt.Sprintf("%T", node)[5:])
+	nodeType := reflect.ValueOf((*ast.Node)(nil)).Type().Elem()
+	nv := reflect.ValueOf(node).Elem()
+	if nv.Kind() == reflect.Struct {
+		fieldCt := nv.NumField()
+		ty := nv.Type()
+		for i := 0; i < fieldCt; i++ {
+			f := ty.Field(i)
+			fv := nv.Field(i)
+			if f.Type.ConvertibleTo(nodeType) {
+				// Don't show nodes since we'll walk into them
+				continue
+			}
+			if f.Type.Kind() == reflect.Slice && f.Type.Elem().ConvertibleTo(nodeType) {
+				// Don't show slices of nodes either
+				continue
+			}
+			if f.Type.AssignableTo(reflect.TypeOf([]byte(nil))) {
+				// Don't show source slices
+				continue
+			}
+			if f.Type.AssignableTo(reflect.TypeOf(ast.WithRange{})) || f.Type.AssignableTo(reflect.TypeOf(source.Range{})) {
+				// Don't show ranges
+				continue
+			}
+			fmt.Fprintf(buf, " %s=%#v", f.Name, fv.Interface())
+		}
+	}
+	buf.WriteString("\n")
+	fmt.Print(buf.String())
+	w.Depth++
+	return true
+}
+
+func (w *walker) ExitNode(node ast.Node) {
+	w.Depth--
+}
