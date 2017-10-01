b7aba6ad0c9b400f39802ba45f7ba9730e55c4b0
Martin Atkins <mart@degeneration.co.uk>
2017-10-01 08:37:37 -0700

    compiler: new package for processing AST into model objects

---
+++ compiler/doc.go
@@ -0,0 +1,4 @@
+// Package compiler contains functions for transforming Cirbo ASTs (objects
+// from the "ast" package) into Cirbo models (objects from the "cbo" and "eval"
+// packages).
+package compiler
