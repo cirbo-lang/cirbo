183d5b6ed34e1fe5819ee099168414dc7ec67bf4
Martin Atkins <mart@degeneration.co.uk>
2017-10-01 18:45:57 -0700

    ast: Import.SymbolName to compute the effective symbol name

--- ast/import.go
+++ ast/import.go
@@ -1,6 +1,8 @@
 package ast
 
 import (
+	"path"
+
 	"github.com/cirbo-lang/cirbo/source"
 )
 
@@ -14,4 +16,11 @@ type Import struct {
 
 func (n *Import) walkChildNodes(cb internalWalkFunc) {
 	// Import is a leaf node, so there are no child nodes to walk
+}
+
+func (n *Import) SymbolName() string {
+	if n.Name != "" {
+		return n.Name
+	}
+	return path.Base(n.Package)
 }
