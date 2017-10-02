9b8b1c81a5da10c158b7c927f905bca7e2842e06
Martin Atkins <mart@degeneration.co.uk>
2017-10-01 18:46:49 -0700

    compiler: beginnings of compilation of packages
    This includes an early version of compilation of statement blocks, though
    it's far from complete.

---
+++ compiler/package.go
@@ -0,0 +1,48 @@
+package compiler
+
+import (
+	"github.com/cirbo-lang/cirbo/ast"
+	"github.com/cirbo-lang/cirbo/cty"
+	"github.com/cirbo-lang/cirbo/source"
+)
+
+// CompilePackage compiles the given package and returns the value exported
+// by it.
+//
+// If the package has an explicit "export" statement then its argument is
+// returned. Otherwise, symbols from the module's symbol table are used to
+// construct and instantiate an object type.
+//
+// An Importer must be provided to specify how to resolve any import statements
+// in the package.
+func CompilePackage(pkg *ast.Package) (cty.Value, source.Diags) {
+	var diags source.Diags
+
+	// First we'll make sure the top-level statements in all of our files
+	// are permitted at the module level.
+	for _, file := range pkg.Files {
+		for _, node := range file.TopLevel {
+			switch tn := node.(type) {
+			case *ast.Assign, *ast.Import, *ast.Export, *ast.Circuit, *ast.Device, *ast.Land, *ast.Pinout:
+				// allowed
+			case *ast.Connection:
+				diags = append(diags, source.Diag{
+					Level:   source.Error,
+					Summary: "Invalid top-level statement",
+					Detail:  "Connection statements may not appear in the top-level scope of a module.",
+					Ranges:  tn.SourceRange().List(),
+				})
+			default:
+				// Generic error message for all other types
+				diags = append(diags, source.Diag{
+					Level:   source.Error,
+					Summary: "Invalid top-level statement",
+					Detail:  "This statement is not allowed in the top-level scope of a module.",
+					Ranges:  tn.SourceRange().List(),
+				})
+			}
+		}
+	}
+
+	return cty.PlaceholderVal, diags
+}
---
+++ compiler/statements.go
@@ -0,0 +1,121 @@
+package compiler
+
+import (
+	"fmt"
+
+	"github.com/cirbo-lang/cirbo/ast"
+	"github.com/cirbo-lang/cirbo/eval"
+	"github.com/cirbo-lang/cirbo/source"
+)
+
+func compileStatements(nodes []ast.Node, parentScope *eval.Scope) (eval.StmtBlock, source.Diags) {
+	var diags source.Diags
+	declRange := map[string]source.Range{}
+	scope := parentScope.NewChild()
+
+	for _, node := range nodes {
+		decl := declForNode(node)
+		if decl.Name == "" {
+			continue
+		}
+
+		if rng, exists := declRange[decl.Name]; exists {
+			diags = append(diags, source.Diag{
+				Level:   source.Error,
+				Summary: "Duplicate declaration",
+				Detail:  fmt.Sprintf("The name %q was already used in the declaration at %s.", decl.Name, rng),
+				Ranges:  decl.Range.List(),
+			})
+			continue
+		}
+
+		scope.Declare(decl.Name)
+		declRange[decl.Name] = decl.Range
+	}
+
+	// TODO: Deal with any implicit device instantiations that may appear
+	// in connection statements. These need to be added as additional
+	// symbols with implied assign statements and then have some special
+	// treatment applied so that when we compile the expr we'll refer to
+	// these additional symbols.
+
+	var stmts []eval.Stmt
+	for _, node := range nodes {
+		stmt, stmtDiags := compileStatement(node, scope)
+		diags = append(diags, stmtDiags...)
+		stmts = append(stmts, stmt)
+	}
+
+	block, blockDiags := eval.MakeStmtBlock(stmts)
+	diags = append(diags, blockDiags...)
+
+	return block, diags
+}
+
+func compileStatement(node ast.Node, scope *eval.Scope) (eval.Stmt, source.Diags) {
+	switch tn := node.(type) {
+	case *ast.Assign:
+		expr, diags := CompileExpr(tn.Value, scope)
+		sym := scope.Get(tn.Name)
+		return eval.AssignStmt(sym, expr, tn.SourceRange()), diags
+	default:
+		panic(fmt.Errorf("%#v cannot be compiled to a statement", node))
+	}
+}
+
+func declForNode(node ast.Node) symbolDecl {
+	switch tn := node.(type) {
+	case *ast.Assign:
+		return symbolDecl{
+			Name:  tn.Name,
+			Range: tn.SourceRange(),
+		}
+	case *ast.Import:
+		return symbolDecl{
+			Name:  tn.SymbolName(),
+			Range: tn.SourceRange(),
+		}
+	case *ast.Attr:
+		return symbolDecl{
+			Name:  tn.Name,
+			Range: tn.SourceRange(),
+		}
+	case *ast.Terminal:
+		return symbolDecl{
+			Name:  tn.Name,
+			Range: tn.SourceRange(),
+		}
+	case *ast.Circuit:
+		return symbolDecl{
+			Name:  tn.Name,
+			Range: tn.DeclRange(),
+		}
+	case *ast.Device:
+		return symbolDecl{
+			Name:  tn.Name,
+			Range: tn.DeclRange(),
+		}
+	case *ast.Land:
+		return symbolDecl{
+			Name:  tn.Name,
+			Range: tn.DeclRange(),
+		}
+	case *ast.Board:
+		return symbolDecl{
+			Name:  tn.Name,
+			Range: tn.DeclRange(),
+		}
+	case *ast.Pinout:
+		return symbolDecl{
+			Name:  tn.Name,
+			Range: tn.DeclRange(),
+		}
+	default:
+		return symbolDecl{}
+	}
+}
+
+type symbolDecl struct {
+	Name  string
+	Range source.Range
+}
